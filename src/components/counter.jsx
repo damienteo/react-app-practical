c45bf629488612a1409252f29b83f8f8c9175790
damienteo <ksteo89@gmail.com>
2019-03-28 12:51:06 +0800

    Dynamic classes and mapping function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: "10px",
+    fontWeight: "bold"
+  };
+
   render() {
-    return <h1>Hello world!</h1>;
+    const { count, tags } = this.state;
+
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+  getBadgeClasses() {
+    const { count } = this.state;
+    let classes = "badge m-2 badge-";
+    classes += count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
