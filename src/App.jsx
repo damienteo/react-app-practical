88e77b35e23769e5bfead4b79cb70c6c5adefe1a
damienteo <ksteo89@gmail.com>
2019-03-28 17:47:36 +0800

    Added NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
 class App extends React.Component {
   render() {
-    return <Counters />;
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
