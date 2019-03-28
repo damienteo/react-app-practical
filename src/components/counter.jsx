7563ca866aeedcf9d73ddfb75332918e9f8c7166
damienteo <ksteo89@gmail.com>
2019-03-28 17:38:13 +0800

    Increment handler changed to single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,35 +1,27 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value
-  };
-
   styles = {
     fontSize: "10px",
     fontWeight: "bold"
   };
 
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
-    const { count, tags } = this.state;
-    console.log(this.props);
+    const { count, tags } = this.props.counter;
+    const { id, value } = this.props.counter;
 
     return (
       <React.Fragment>
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -39,14 +31,14 @@ class Counter extends Component {
   }
 
   getBadgeClasses() {
-    const { count } = this.state;
+    const { value } = this.props.counter;
     let classes = "badge m-2 badge-";
-    classes += count === 0 ? "warning" : "primary";
+    classes += value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
